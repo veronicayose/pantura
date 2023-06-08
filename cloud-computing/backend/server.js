b22794470247aaa542fd1f00b944dd66e376c880
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2023-06-08 20:33:25 +0700

    add cors package

--- cloud-computing/backend/server.js
+++ cloud-computing/backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('', routes);
 
