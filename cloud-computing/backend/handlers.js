f87eece37ec548750affaa56731e64cd955df37d
davidnathanio <davidnathanio21@gmail.com>
2023-06-11 16:42:21 +0700

    added image upload to GCS

--- cloud-computing/backend/handlers.js
+++ cloud-computing/backend/handlers.js
@@ -1,21 +1,19 @@
 import db from './connection.js';
 import response from './response.js';
+import ImgUpload from './imgUpload.js'; 
+import multer from 'multer';
 
-// Show all reports
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// Show all reports and based on search key
 const getAllReports = (req, res) => {
-    const query = `SELECT laporan.id, laporan.lokasi, laporan.keterangan, laporan.foto, laporan.status_penanganan, laporan.status_laporan, laporan.tingkat_kerusakan, laporan.tanggal_buat, laporan.tanggal_edit, users.nama as pelapor FROM laporan JOIN users ON laporan.user_id = users.id;`;
-    db.query(query, (error, result) => {
-        response(200, result, "success", res);
-    })    
-};
-
-// Show all report based on search key
-const getSearchReports = (req, res) => {
-    let { search } = req.params;
+    let { search } = req.body;
     if (search === undefined){
         search = "";
     }
-    const query = `SELECT laporan.id, laporan.lokasi, laporan.keterangan, laporan.foto, laporan.status_penanganan, laporan.status_laporan, laporan.tingkat_kerusakan, laporan.tanggal_buat, laporan.tanggal_edit, users.nama as pelapor FROM laporan JOIN users ON laporan.user_id = users.id WHERE laporan.lokasi LIKE "%${search}%" OR laporan.keterangan LIKE "%${search}%" OR users.nama LIKE "%${search}%";`;
+    const query = `SELECT laporan.id, laporan.lokasi, laporan.keterangan, laporan.foto, laporan.status_penanganan, laporan.status_laporan, laporan.tingkat_kerusakan, laporan.tanggal_buat, laporan.tanggal_edit, users.nama as pelapor FROM laporan JOIN users ON laporan.user_id = users.id WHERE laporan.lokasi LIKE "%${search}%" OR laporan.keterangan LIKE "%${search}%";`;
     db.query(query, (error, result) => {
         response(200, result, "success", res);
     })    
@@ -59,28 +57,76 @@ const getDetailReport = (req, res) => {
 
 // Update report based on report id
 const updateReport = (req, res) => {
-    const tanggal_edit = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    const { id } = req.params;
-    const { lokasi, keterangan, foto, status_penanganan, status_laporan, tingkat_kerusakan} = req.body
-    const query = `UPDATE laporan SET lokasi = '${lokasi}', tanggal_edit = '${tanggal_edit}', foto = '${foto}', keterangan = '${keterangan}', status_penanganan = '${status_penanganan}', status_laporan = '${status_laporan}', tingkat_kerusakan = '${tingkat_kerusakan}' WHERE id = ${id}`;
-    
-    db.query(query, (error, result) => {
-        if (error) {
+    upload.single('attachment')(req, res, (error) => {
+      if (error) {
+        console.error(error);
+        response(500, "Error uploading file", "Failed", res);
+        return;
+      }
+  
+      const { id } = req.params;
+      const { lokasi, keterangan, status_penanganan, status_laporan, tingkat_kerusakan } = req.body;
+      const file = req.file; // Get the uploaded file
+  
+      // Check if a file was uploaded
+      if (file) {
+        // Upload the file to GCS using the ImgUpload module
+        ImgUpload.uploadToGcs(file, (error, imageUrl) => {
+          if (error) {
+            console.error(error);
+            response(500, "Error uploading file to GCS", "Failed", res);
+            return;
+          }
+  
+          // Update the record in the database with the new image URL
+          const query = `UPDATE laporan SET lokasi = ?, keterangan = ?, foto = ?, status_penanganan = ?, status_laporan = ?, tingkat_kerusakan = ? WHERE id = ?`;
+          const values = [lokasi, keterangan, imageUrl, status_penanganan, status_laporan, tingkat_kerusakan, id];
+  
+          db.query(query, values, (error, result) => {
+            if (error) {
+              console.error(error);
+              response(500, "Invalid", "Error", res);
+              return;
+            }
+  
+            if (result?.affectedRows) {
+              const data = {
+                isSuccess: result.affectedRows,
+                message: result.message,
+              };
+              response(200, data, "Update report successfully", res);
+            } else {
+              response(404, "Report not found", "Failed", res);
+            }
+          });
+        });
+      } else {
+        // No file was uploaded, update the record without changing the image
+        const query = `UPDATE laporan SET lokasi = ?, keterangan = ?, status_penanganan = ?, status_laporan = ?, tingkat_kerusakan = ? WHERE id = ?`;
+        const values = [lokasi, keterangan, status_penanganan, status_laporan, tingkat_kerusakan, id];
+  
+        db.query(query, values, (error, result) => {
+          if (error) {
             console.error(error);
             response(500, "Invalid", "Error", res);
             return;
-        }
-        if (result?.affectedRows){
+          }
+  
+          if (result?.affectedRows) {
             const data = {
-                isSuccess: result.affectedRows,
-                message: result.message,
-            }
+              isSuccess: result.affectedRows,
+              message: result.message,
+            };
             response(200, data, "Update report successfully", res);
-        } else {
-            response("404", "Report not Found", "Failed", res);
-        }
+          } else {
+            response(404, "Report not found", "Failed", res);
+          }
+        });
+      }
     });
-};
+  };
+  
+  
 
 // Delete report based on report id
 const deleteReport = (req, res) => {
@@ -107,28 +153,55 @@ const deleteReport = (req, res) => {
 
 // Create Report
 const createReport = (req, res) => {
-    const tanggal_buat = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    const tanggal_edit = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    const { lokasi, keterangan, foto, status_penanganan, status_laporan, tingkat_kerusakan, user_id } = req.body
-    const query = `INSERT INTO laporan (lokasi, keterangan, foto, status_penanganan, status_laporan, tingkat_kerusakan, user_id, tanggal_buat, tanggal_edit) VALUES ('${lokasi}', '${keterangan}', '${foto}', '${status_penanganan}', '${status_laporan}', '${tingkat_kerusakan}', ${user_id}, ${tanggal_buat}, ${tanggal_edit});`;
-    
-    db.query(query, (error, result) => {
+  upload.single('attachment')(req, res, (error) => {
+    if (error) {
+      console.error(error);
+      response(500, "Error uploading file", "Failed", res);
+      return;
+    }
+
+    const { lokasi, keterangan, status_penanganan, status_laporan, tingkat_kerusakan, user_id } = req.body;
+    const file = req.file;
+
+    if (!file) {
+      response(400, "No file uploaded", "Failed", res);
+      return;
+    }
+
+    // Upload the file to GCS using the ImgUpload module
+    ImgUpload.uploadToGcs(file, (error, imageUrl) => {
+      if (error) {
+        console.error(error);
+        response(500, "Error uploading file to GCS", "Failed", res);
+        return;
+      }
+
+      // Insert the record into the database with the image URL
+      const query = `INSERT INTO laporan (lokasi, keterangan, foto, status_penanganan, status_laporan, tingkat_kerusakan, user_id) VALUES (?, ?, ?, ?, ?, ?, ?);`;
+      const values = [lokasi, keterangan, imageUrl, status_penanganan, status_laporan, tingkat_kerusakan, user_id];
+
+      db.query(query, values, (error, result) => {
         if (error) {
-            console.error(error);
-            response(500, "Invalid", "Error", res);
-            return;
+          console.error(error);
+          response(500, "Invalid", "Error", res);
+          return;
         }
-        if (result?.affectedRows){
-            const data = {
-                isSuccess: result.affectedRows,
-                message: result.message,
-            }
-            response(200, result, "Report created successfully", res);
+
+        if (result?.affectedRows) {
+          const data = {
+            isSuccess: result.affectedRows,
+            message: result.message,
+          };
+          response(200, result, "Report created successfully", res);
         } else {
-            response("404", "Failed to create user", "Failed", res);
+          response(404, "Failed to create report", "Failed", res);
         }
+      });
     });
-}
+  });
+};
+
+
 
 // Create Users
 const createUser = (req, res) => {
@@ -148,20 +221,22 @@ const createUser = (req, res) => {
             }
             response(200, result, "User created successfully", res);
         } else {
-            response(404, "Failed to create user", "Failed", res);
+            response("404", "Failed to create user", "Failed", res);
         }
     });
 }
 
 // Login Users
 const loginUser = (req, res) => {
-    const { email, password, role } = req.params;
-    const query = `SELECT * FROM users WHERE email LIKE "${email}" AND password LIKE "${password}" AND role LIKE "${role}";`;
+    const { email, password } = req.body;
+    const query = `SELECT * FROM users WHERE email LIKE "${email}" AND password LIKE "${password}";`;
     db.query(query, (error, result) => {
         if (result.length > 0){
             response(200, result, "success", res);
+            // return true;
         } else {
             response(404, "User not found", "failed", res);
+            // return false;
         }
     })
 };
@@ -177,5 +252,4 @@ export {
     createReport,
     createUser,
     loginUser,
-    getSearchReports
 }
