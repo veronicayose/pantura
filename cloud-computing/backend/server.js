0a7a1d23956739f70b8533b9171e98beff4b52bb
Veronica Yose Ardilla <80503527+veronicayose@users.noreply.github.com>
2023-06-16 21:27:34 +0700

    Revert "update"
    This reverts commit ae3b35e32076f5a3fb992f3eec01a2bd4ae4016d.

--- cloud-computing/backend/server.js
+++ cloud-computing/backend/server.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import routes from './routes.js';
 import cors from 'cors';
-import * as dotenv from 'dotenv';
 
 const app = express();
-
-dotenv.config();
-const PORT = process.env.PORT || 8080;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
